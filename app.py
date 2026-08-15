f2b670facaa424b58774c4bc99b6b2a06ff5846a
chococh085 <treekat632@gmail.com>
2026-08-16 00:35:18 +0800

    Initialize Flask app with home and test routes

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app=Flask(__name__)#__name__代表目前執行的模組
+
+@app.route("/")#函式的裝飾 (Decorator): 以函式為基礎,提供附加的功能
+def home():
+    return "Hello Flask 2"
+@app.route("/test")
+def test():
+    return "This is test"
+if __name__=="__main__":  #如果以主程式執行
+   app.run() #立刻啟動伺服器
